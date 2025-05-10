fcaf57bf70930076cb3be47d491bc6aa46735289
shoh <shberdikobilov@gmail.com>
2025-05-10 22:33:36 +0200

    Part 2c added Read and Print and adjusted student.cpp

---
+++ Part_2c/include/io.h
@@ -0,0 +1,29 @@
+#ifndef MAPRA_IO_H_
+#define MAPRA_IO_H_
+
+#include <fstream>
+#include <iostream>
+#include <vector>
+
+namespace mapra {
+
+// Reads all T’s from ifs into the back of array via operator>>
+template <typename T>
+void Read(std::ifstream& ifs, std::vector<T>& array) {
+  T tmp;
+  while (ifs >> tmp) {
+    array.push_back(tmp);
+  }
+}
+
+// Prints each element of array via operator<<, one per line
+template <typename T>
+void Print(std::ostream& os, const std::vector<T>& array) {
+  for (const auto& x : array) {
+    os << x << "\n";
+  }
+}
+
+}  // namespace mapra
+
+#endif  // MAPRA_IO_H_
--- Part_2c/src/student.cpp
+++ Part_2c/src/student.cpp
@@ -1,6 +1,6 @@
 // Copyright (c) 2022, The MaPra Authors.
 
-#include "student.h"
+#include "include/student.h"
 
 // Eingabeoperator ">>"
 std::istream& mapra::operator>>(std::istream& s, mapra::Student& a) {
@@ -10,19 +10,23 @@ std::istream& mapra::operator>>(std::istream& s, mapra::Student& a) {
 
 // Ausgabeoperator "<<"
 std::ostream& mapra::operator<<(std::ostream& s, const mapra::Student& a) {
-  // hier den Ausgabeoperator definieren
+  s << a.first_name << " " << a.last_name << " " << a.matr_nr << " " << a.grade;
+  return s;
 }
 
 // Vergleichsoperator "<"
-bool mapra::operator<(const mapra::Student&, const mapra::Student&) {
-  // hier fehlt was
+bool mapra::operator<(const mapra::Student& lhs, const mapra::Student& rhs) {
+  if (lhs.last_name < rhs.last_name) return true;
+  if (rhs.last_name < lhs.last_name) return false;
+  return lhs.first_name < rhs.first_name;
 }
 
 // Vergleichsoperatoren "==" bzw. "!="
-bool mapra::operator==(const mapra::Student&, const mapra::Student&) {
-  // hier fehlt was
+bool mapra::operator==(const mapra::Student& lhs, const mapra::Student& rhs) {
+  return lhs.first_name == rhs.first_name && lhs.last_name == rhs.last_name &&
+         lhs.matr_nr == rhs.matr_nr && lhs.grade == rhs.grade;
 }
 
-bool mapra::operator!=(const mapra::Student&, const mapra::Student&) {
-  // hier fehlt was
+bool mapra::operator!=(const mapra::Student& lhs, const mapra::Student& rhs) {
+  return !(lhs == rhs);
 }
